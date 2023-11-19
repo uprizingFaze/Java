f4e2aa40feada95f0a28c0f2422c5e7f7f462bc2
uprizingFaze <uprizing.faze@protonmail.com>
2023-11-19 11:54:52 -0500

    Second commit

--- practica/src/main/java/org/example/Main.java
+++ practica/src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
+//Definicion de clase cualquier nombre
 public class Main {
+    //definicion de metodo, esto define *main()*
+    //El ; indica que el compilador a finalizado una sentencia
     public static void main(String[] args) {
+        //Calcular perimetro de un circunferencia
+        final double PI = 3.14;
+        double radio = 25.0, perimetro, perimetro2;
 
-        System.out.println("Hola mundo");
+        perimetro = 2 * PI * radio;
+        System.out.println("El perimetro del radio " + radio + " es " + perimetro);
+
     }
 }
