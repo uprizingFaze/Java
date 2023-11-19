1eeae599dc2e50a0d6c67d637966494c16154cb9
uprizingFaze <uprizing.faze@protonmail.com>
2023-11-19 15:22:18 -0500

    Quinto commit

--- practica/src/main/java/org/example/Main.java
+++ practica/src/main/java/org/example/Main.java
@@ -13,6 +13,9 @@ public class Main {
         perimetro = 2 * PI * radio;
         System.out.println("El perimetro del radio " + radio + " es " + perimetro);
 
+        Vehiculo vehiculo = new Vehiculo("123-ABC", "BMW", "2023", "Negro", 100.1);
+        Vehiculo.Vehiculo();
+
     }
 
 
@@ -29,6 +32,7 @@ public class Main {
         Circulo() {
             circulo1 = new Circulo();
             circulo2 = new Circulo();
+
         }
 
     }
@@ -43,6 +47,76 @@ public class Main {
         double tarifa;
         boolean disponible;
 
+        public String getMatricula() {
+            return matricula;
+        }
+
+        public void setMatricula(String matricula) {
+            this.matricula = matricula;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public double getTarifa() {
+            return tarifa;
+        }
+
+        public void setTarifa(double tarifa) {
+            this.tarifa = tarifa;
+        }
+
+        public boolean isDisponible() {
+            return disponible;
+        }
+
+        public void setDisponible(boolean disponible) {
+            this.disponible = disponible;
+        }
+
+        //Metodo constructor de la clase vehiculo
+        public Vehiculo(String matricula, String marca, String modelo, String color, double tarifa) {
+           /* Vehiculo matricula;
+            matricula = new Vehiculo();*/
+            this.matricula = matricula;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.color  = color;
+            this.tarifa = tarifa;
+
+            Vehiculo vehiculo1 = new Vehiculo("123-ABC", "BMW", "2023", "Negro", 100.1);
+            Vehiculo vehiculo2 = new Vehiculo("456-DEF", "MERCEDES", "2023", "Blanco", 20.1);
+
+            System.out.println("Matricula: " + vehiculo2.matricula);
+
+
+        }
+
+
     }
+    Vehiculo vehiculo1 = new Vehiculo("123-ABC", "BMW", "2023", "Negro", 100.1);
+    Vehiculo vehiculo2 = new Vehiculo("456-DEF", "MERCEDES", "2023", "Blanco", 20.1);
+System.out.print("Matricula: " + vehiculo2.getMatricula());
 
 }
