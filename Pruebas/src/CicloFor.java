a3c730ce46ff2e441cf578a3636fb660664a6cd3
uprizingFaze <uprizing.faze@protonmail.com>
2023-11-30 11:41:24 -0500

    FizzBuzz

--- Pruebas/src/CicloFor.java
+++ Pruebas/src/CicloFor.java
@@ -1,8 +1,20 @@
 public class CicloFor {
     public void run() {
         System.out.println("Hello world!");
-        for (int i = 500; i <= 1000; i = i + 2) {
-            System.out.println(i);
-        }
+
+        for (int i = 1; i <= 100; i = i + 1) {
+            if (i % 3 == 0 && i % 5 ==0){
+                System.out.println("fizzbuzz: "+i);
+            } else if (i % 3 ==0) {
+                System.out.println("fizz: "+ i);
+            } else if (i % 5 ==0) {
+                System.out.println("buzz: "+ i);
+
+            } else {
+                System.out.println(i);
+            }
+
+            }
+
     }
 }
