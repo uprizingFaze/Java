06e66006eb7e0488e90c0b335d966915b543a7c7
uprizingFaze <uprizing.faze@protonmail.com>
2023-11-30 11:25:08 -0500

    Clase funcional

--- Pruebas/src/CicloFor.java
+++ Pruebas/src/CicloFor.java
@@ -1,5 +1,5 @@
 public class CicloFor {
-    public static void main(String args[]) {
+    public void run() {
         System.out.println("Hello world!");
         for (int i = 500; i <= 1000; i = i + 2) {
             System.out.println(i);
--- Pruebas/src/Main.java
+++ Pruebas/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        CicloFor for1;
+        CicloFor for1 = new CicloFor();
+        for1.run();
     }
 }
