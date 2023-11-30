da5727344931d4f46025257cc374d0e5fc949656
uprizingFaze <uprizing.faze@protonmail.com>
2023-11-30 11:20:27 -0500

    Test de pruebas tecnicas

---
+++ Pruebas/src/CicloFor.java
@@ -0,0 +1,8 @@
+public class CicloFor {
+    public static void main(String args[]) {
+        System.out.println("Hello world!");
+        for (int i = 500; i <= 1000; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
